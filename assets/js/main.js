7c332828ed8c9d8ceeb11f3e2bb114a51f267a4b
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-28 18:25:06 +0200

    aggiunta if

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,24 @@ next.addEventListener(`click`, function(){
     
     let changeActive = activeItem.nextElementSibling
 
+    if(activeItem.classList.contains(`last`)){
+        changeActive = document.querySelector(`.item.first`)
+    }
+
+    activeItem.classList.remove(`active`)
+
+    changeActive.classList.add(`active`)
+})
+
+prev.addEventListener(`click`, function(){
+    let activeItem = document.querySelector(`.item.active`)
+    
+    let changeActive = activeItem.previousElementSibling
+
+    if(activeItem.classList.contains(`first`)){
+        changeActive = document.querySelector(`.item.last`)
+    }
+
     activeItem.classList.remove(`active`)
 
     changeActive.classList.add(`active`)
